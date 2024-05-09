9918e51ef842ef9624d58c047fe988feda458521
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-05-09 14:29:23 -0400

    WIP

---
+++ src/CabanaPD_Neighbor.hpp
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * Copyright (c) 2022-2023 by Oak Ridge National Laboratory                 *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of CabanaPD. CabanaPD is distributed under a           *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#include "mpi.h"
+#include <iostream>
+#include <sstream>
+
+namespace CabanaPD
+{
+
+template <typename NeighborType>
+class Neighbors
+{
+    Neighbors( x, 0, particles->n_local, force_model.delta, 1.0, mesh_min,
+               mesh_max )
+};
+} // namespace CabanaPD
